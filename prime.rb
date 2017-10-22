3efe728cccc183a91ddec2614617d4d73e148a6f
James Royer <walkingalchemy@gmail.com>
2017-10-22 17:57:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  (2..(number ** 1/2)).each do |i|
+    if number % i == 0
+      return false
+    end
+  end
+  true
+end
