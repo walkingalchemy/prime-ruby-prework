354927b4739108cd2fe9d4d40eff06606642c550
James Royer <walkingalchemy@gmail.com>
2017-10-22 17:37:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  if number < 0
+    false
+  end
+  if number in 1..3
+    true
+  else
+    (4..(number ** 1/2)).each do |i|
+      if number % i != 0
+        return false
+      end
+    end
+    true
+  end
+end
