1abc9ad5d68fa3ee33f612d84622d065d04d89d7
James Royer <walkingalchemy@gmail.com>
2017-10-22 17:52:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,12 @@
 # Add  code here!
 def prime?(number)
-  if number <= 0
+  if number <= 1
     return false
   end
-  if (1..3).include?(number)
-    true
-  else
-    (4..(number ** 1/2)).each do |i|
-      if number % i == 0
-        return false
-      end
+  (2..(number ** 1/2)).each do |i|
+    if number % i == 0
+      return false
     end
-    true
   end
+  true
 end
